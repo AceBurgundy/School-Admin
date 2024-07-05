8c3efe5928fe806a8411f0360ee1b3bb7515206a
rakinu21 <rakinkinra@gmail.com>
2024-07-05 10:25:32 +0800

    creating pages for department andexamdate file

---
+++ examdate.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Student</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="./styles/examdate.css">
+  <link rel="stylesheet" href="./styles/global.css">
+  <script type="module" src="./scripts/examdate.js"></script>
+</head>
+
+<body>
+  <button id="create-new-dateTaken-button" class="btn btn-primary">Create</button>
+
+  <form id="new-datetaken-form">
+
+    <div class="form-group">
+      <label for="dateTaken">Date Taken</label>
+      <input type="date" class="form-control" id="dateTaken" placeholder="dateTaken">
+    
+    </div>
+
+  
+
+  </form>
+
+</body>
+
+</html>

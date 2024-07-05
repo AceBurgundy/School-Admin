b328c6674aa4556172934af0b2c730c1c19334ed
AceBurgundy <samadriansabalo99@gmail.com>
2024-07-05 10:45:36 +0800

    Added header, sidebar.

--- school.php
+++ school.php
@@ -1,35 +1,38 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
-
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Student</title>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-  <link rel="stylesheet" href="./styles/school.css">
-  <link rel="stylesheet" href="./styles/global.css">
-  <script type="module" src="./scripts/school.js"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
-
 <body>
-  <button id="create-new-school-button" class="btn btn-primary">Create</button>
 
-  <form id="new-school-form">
+  TANGALON NI
+-->
 
-    <div class="form-group">
-      <label for="name">Name</label>
-      <input type="text" class="form-control" id="name" placeholder="First name">
-    </div>
+  <?php
+    $page = 'school';
+    require 'header.php';
+  ?>
+  <section>
+    <button id="create-new-school-button" class="btn btn-primary">Create</button>
 
-    <div class="form-group">
-      <label for="address">Address</label>
-      <input type="text" class="form-control" id="address" name="address" placeholder="Address" maxlength="255">
-    </div>
+    <form id="new-school-form">
 
-    <button type="submit" class="btn btn-primary">Submit</button>
+      <div class="form-group">
+        <label for="name">Name</label>
+        <input type="text" class="form-control" id="name" placeholder="First name">
+      </div>
 
-  </form>
+      <div class="form-group">
+        <label for="address">Address</label>
+        <input type="text" class="form-control" id="address" name="address" placeholder="Address" maxlength="255">
+      </div>
 
+      <button type="submit" class="btn btn-primary">Submit</button>
+
+    </form>
+  </section>
 </body>
 
 </html>
---
+++ sidebar.php
@@ -0,0 +1,155 @@
+<nav class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark">
+  <a href="" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+    <svg class="bi me-2" width="40" height="32">
+      <use xlink:href="#bootstrap"></use>
+    </svg>
+    <span class="fs-4">RMMC Admin</span>
+  </a>
+  <hr>
+  <ul class="nav nav-pills flex-column mb-auto">
+    <li class="nav-item">
+      <a href="admin.php" class="nav-link active" aria-current="page">
+        <svg class="bi me-2" width="16" height="16">
+          <use xlink:href="#home"></use>
+        </svg>
+        Admin
+      </a>
+    </li>
+    <li>
+      <a href="college.php" class="nav-link text-white">
+        <svg class="bi me-2" width="16" height="16">
+          <use xlink:href="#speedometer2"></use>
+        </svg>
+        College
+      </a>
+    </li>
+    <li>
+      <a href="contactnumber.php" class="nav-link text-white">
+        <svg class="bi me-2" width="16" height="16">
+          <use xlink:href="#table"></use>
+        </svg>
+        Contact Number
+      </a>
+    </li>
+    <li>
+      <a href="course.php" class="nav-link text-white">
+        <svg class="bi me-2" width="16" height="16">
+          <use xlink:href="#grid"></use>
+        </svg>
+        Course
+      </a>
+    </li>
+    <li>
+      <a href="coursecurriculum.php" class="nav-link text-white">
+        <svg class="bi me-2" width="16" height="16">
+          <use xlink:href="#people-circle"></use>
+        </svg>
+        Course Curriculum
+      </a>
+    </li>
+    <li>
+      <a href="coursereview.php" class="nav-link text-white">
+        <svg class="bi me-2" width="16" height="16">
+          <use xlink:href="#people-circle"></use>
+        </svg>
+        Course Review
+      </a>
+    </li>
+    <li>
+      <a href="department.php" class="nav-link text-white">
+        <svg class="bi me-2" width="16" height="16">
+          <use xlink:href="#people-circle"></use>
+        </svg>
+        Department
+      </a>
+    </li>
+    <li>
+      <a href="examdate.php" class="nav-link text-white">
+        <svg class="bi me-2" width="16" height="16">
+          <use xlink:href="#people-circle"></use>
+        </svg>
+        Exam Date
+      </a>
+    </li>
+    <li>
+      <a href="goal.php" class="nav-link text-white">
+        <svg class="bi me-2" width="16" height="16">
+          <use xlink:href="#people-circle"></use>
+        </svg>
+        Goal
+      </a>
+    </li>
+    <li>
+      <a href="goal_and_objective.php" class="nav-link text-white">
+        <svg class="bi me-2" width="16" height="16">
+          <use xlink:href="#people-circle"></use>
+        </svg>
+        Goal and Objective
+      </a>
+    </li>
+    <li>
+      <a href="instructor.php" class="nav-link text-white">
+        <svg class="bi me-2" width="16" height="16">
+          <use xlink:href="#people-circle"></use>
+        </svg>
+        Instructor
+      </a>
+    </li>
+    <li>
+      <a href="news.php" class="nav-link text-white">
+        <svg class="bi me-2" width="16" height="16">
+          <use xlink:href="#people-circle"></use>
+        </svg>
+        News
+      </a>
+    </li>
+    <li>
+      <a href="newsauthor.php" class="nav-link text-white">
+        <svg class="bi me-2" width="16" height="16">
+          <use xlink:href="#people-circle"></use>
+        </svg>
+        News Author
+      </a>
+    </li>
+    <li>
+      <a href="scholarship.php" class="nav-link text-white">
+        <svg class="bi me-2" width="16" height="16">
+          <use xlink:href="#people-circle"></use>
+        </svg>
+        Scholarship
+      </a>
+    </li>
+    <li>
+      <a href="school.php" class="nav-link text-white">
+        <svg class="bi me-2" width="16" height="16">
+          <use xlink:href="#people-circle"></use>
+        </svg>
+        School
+      </a>
+    </li>
+    <li>
+      <a href="student.php" class="nav-link text-white">
+        <svg class="bi me-2" width="16" height="16">
+          <use xlink:href="#people-circle"></use>
+        </svg>
+        Student
+      </a>
+    </li>
+  </ul>
+  <hr>
+  <div class="dropdown">
+    <a href="".php class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+      <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
+      <strong>mdo</strong>
+    </a>
+    <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
+      <li><a class="dropdown-item" href="#">New project...</a></li>
+      <li><a class="dropdown-item" href="#">Settings</a></li>
+      <li><a class="dropdown-item" href="#">Profile</a></li>
+      <li>
+        <hr class="dropdown-divider">
+      </li>
+      <li><a class="dropdown-item" href="#">Sign out</a></li>
+    </ul>
+  </div>
+</nav>

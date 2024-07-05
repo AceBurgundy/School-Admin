2fcb16bb0ad7e1477e5abcdb0aeed762850af736
kissanima <kissanima@gmail.com>
2024-07-05 11:25:59 +0800

    news and scholarship fixes

--- scholarship.php
+++ scholarship.php
@@ -1,22 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Student</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    
-        <!-- CSS -->
-    <link rel="stylesheet" href="./styles/scholarship.css">
-    <link rel="stylesheet" href="./styles/global.css">
-
-    <!-- MODULE -->
-    <script type="module" src="./scripts/scholarship.js"></script>
-</head>
-
-<body>
+<?php
+    $page = 'scholarship';
+    require 'header.php';
+  ?>
+    <section>
     <button id="create-new-scholarship-button" class="btn btn-primary">Create</button>
 
     <form id="new-scholarship-form">
@@ -30,7 +16,7 @@
 
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
-
+    </section>
 </body>
 
 </html>

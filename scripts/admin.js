639c3affc094916fa643cd68f0583d817b4349e1
Kirt_12 <roxaskirt21@gmail.com>
2024-07-05 15:57:22 +0800

    ginaayos ang sulod sa admin folder

--- scripts/admin.js
+++ scripts/admin.js
@@ -1,3 +1,6 @@
+
+import { makeToastNotification } from './helper.js';
+
 const createStudentToggle = document.getElementById('create-new-admin-button');
 const newStudentForm = document.getElementById('new-admin-form');
 
@@ -6,12 +9,35 @@ createStudentToggle.onclick = () => newStudentForm.classList.toggle('active');
 newStudentForm.onsubmit = event => {
   event.preventDefault();
 
-  const formData = {
+  const formValues = {
     username: document.getElementById('username').value,
     birthdate: document.getElementById('birthdate').value,
     email: document.getElementById('email').value,
     password: document.getElementById('password').value,
   };
 
-  console.log(formData);
+  const formData = new FormData();
+
+  formData.append("username", formValues['username']);
+  formData.append("birthdate", formValues['birthdate']);
+  formData.append("email", formValues['email']);
+  formData.append("password", formValues['password']);
+
+  fetch("views/admin/create.php", {
+      method: "POST",
+      body: formData,
+  })
+    .then(response => response.json())
+    .then(data => {
+      if (data.status === "error") {
+        makeToastNotification(data.message);
+      }
+
+      if (data.status === "success") {
+        makeToastNotification(data.message);
+        // leave empty
+      }
+    })
+    .catch(error => console.error("Error:", error));
+  
 }

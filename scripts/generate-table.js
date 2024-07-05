b962d055fe3cf3a85debecea3487ac832a500c83
Kirt_12 <roxaskirt21@gmail.com>
2024-07-05 16:21:49 +0800

    adding table_headers and headerList

--- scripts/generate-table.js
+++ scripts/generate-table.js
@@ -1,30 +1,30 @@
-export const createTable = dataList => {
-  let tableHeader = [];
+export const createTable = (headerList, dataList) => {
+  const section = document.body.querySelector("section");
 
-  if (dataList.length > 0) {
-    tableHeader = Object.keys(dataList[0]);
-  }
-
-  const section = document.body.querySelector('section');
-
-  section.innerHTML += /* html */`
+  section.innerHTML += /* html */ `
     <div id='table-container'>
       <table class="table table-dark">
         <thead>
           <tr>
-            ${tableHeader.map(title => `<td>${title}</td>`).join('')}
+            ${
+              headerList && headerList.length > 0
+                ? headerList.map((title) => `<td>${title}</td>`).join("")
+                : ""
+            }
           </tr>
         </thead>
         <tbody>
-          ${
-            dataList.map(data => /* html */`
+          ${dataList
+            .map(
+              (data) => /* html */ `
               <tr>
-                ${tableHeader.map(key => `<td>${data[key]}</td>`).join('')}
+                ${tableHeader.map((key) => `<td>${data[key]}</td>`).join("")}
               </tr>
-            `).join('')
-          }
+            `
+            )
+            .join("")}
         </tbody>
       </table>
     </div>
   `;
-}
+};

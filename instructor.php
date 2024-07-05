cd734573862f32a86bd9c800a759d69ce5badbe7
VirHus <lesterjohn623@gmail.com>
2024-07-05 10:17:18 +0800

    instructor.php updated

--- instructor.php
+++ instructor.php
@@ -43,26 +43,23 @@
 
         <div class="form-group">
             <label for="facebooklinkId">Facebook</label>
-            <input type="text" class="form-control" id="facebookLinkId" name="facebook_link_id" placeholder="Facebook Link"
-                maxlength="255">
+            <input type="url" class="form-control" id="facebookLinkId" name="facebook_link_id" placeholder="Facebook Link">
         </div>
 
         <div class="form-group">
             <label for="twitterLinkId">Twitter</label>
-            <input type="text" class="form-control" id="twitterLinkId" name="twitter_link_id" placeholder="Twitter Link" 
-                maxlength="255">
+            <input type="url" class="form-control" id="twitterLinkId" name="twitter_link_id" placeholder="Twitter Link" >
         </div>
 
         <div class="form-group">
             <label for="linkedinLinkId">Linkedin</label>
-            <input type="text" class="form-control" id="linkedinLinkId" name="linkedin_link_id"placeholder="Linkedin Link" 
-                maxlength="255">
+            <input type="url" class="form-control" id="linkedinLinkId" name="linkedin_link_id" placeholder="Linkedin Link" >
         </div>
 
         <div class="form-group">
             <label for="instagranLinkId">Instagram Link</label>
-            <input type="text" class="form-control" id="instagramLinkId" name="instagran_link_id"
-                placeholder="Instagram Link" maxlength="255">
+            <input type="url" class="form-control" id="instagramLinkId" name="instagran_link_id"
+                placeholder="Instagram Link">
                 <small id="emailHelp" class="form-text text-muted">We'll never share your personal information with anyone else.</small>
         </div>
 

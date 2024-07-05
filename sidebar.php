65fa1b840d42b0d373fb8a68e93733b806f8390c
AceBurgundy <samadriansabalo99@gmail.com>
2024-07-05 22:32:42 +0800

    Fixed dropdown refreshing when clicked.

--- sidebar.php
+++ sidebar.php
@@ -138,7 +138,7 @@
   </ul>
   <hr>
   <div class="dropdown">
-    <a href="".php class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+    <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
       <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
       <strong>mdo</strong>
     </a>

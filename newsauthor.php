b3cf716bcf9d0c9e79c84a468c353b2bef9c7788
kissanima <kissanima@gmail.com>
2024-07-11 16:37:47 +0800

    add import image to authornews

--- newsauthor.php
+++ newsauthor.php
@@ -11,6 +11,10 @@ require 'header.php';
     <div class="form-group">
       <label for="newsauthor">News Author</label>
       <input type="text" class="form-control" id="newsauthor" placeholder="News Author">
+
+      <br>
+      <label for="profileImage">Profile Image</label>
+      <input type="file" class="form-control-file" id="profileImage" name="profileImage">
     </div>
 
     <button type="submit" class="btn btn-primary">Submit</button>
